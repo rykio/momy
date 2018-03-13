fe3af1cf59b9036d5a8a857848a99da67ad13f38
rykio <torykio@gmail.com>
2018-03-13 20:47:41 +0800

    tailor for special usage.

--- bin/momy.js
+++ bin/momy.js
@@ -4,13 +4,33 @@
 
 const Tailer = require('../lib/tailer.js')
 const fs = require('fs')
-
 const DEFAULT_CONFIG_PATH = 'momyfile.json'
-const refresh = process.argv.some(c => c === '--import')
-const finder = (p, c, i, a) => c === '--config' && a[i + 1] ? a[i + 1] : p
-const file = process.argv.reduce(finder, DEFAULT_CONFIG_PATH)
-const config = JSON.parse(fs.readFileSync(process.cwd() + '/' + file))
-const tailer = new Tailer(config)
 
-if (refresh) tailer.importAndStart()
-else tailer.start()
+/**
+ * Momy
+ * @class
+ */
+class Momy {
+
+  /**
+   * Constructor
+   * @param {string} config_file - configulation filename
+   */
+  constructor(config_file) {
+    console.log(config_file)
+    const config_path =  config_file || DEFAULT_CONFIG_PATH;
+    this.config = JSON.parse(fs.readFileSync(process.cwd() + '/' + config_path))
+    console.log(this.config)
+
+    this.tailer = new Tailer(this.config)
+  }
+
+  /**
+   * Start import and tailing
+   */
+  start() {
+    this.tailer.importAndStart()
+  }
+}
+
+module.exports = Momy
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict'
 
-module.exports = require('./lib/tailer.js')
+module.exports = require('./bin/momy.js')
